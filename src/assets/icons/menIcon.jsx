60b25967c5c2dbbc3578cc5456a0c26684fdd1a4
Alisher <abdirimov110@gmail.com>
2023-10-03 14:38:05 +0500

    add header modals

---
+++ src/assets/icons/menIcon.jsx
@@ -0,0 +1,43 @@
+const MenIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={26}
+    height={26}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#E3F1FF"
+      d="M13 26c7.18 0 13-5.82 13-13S20.18 0 13 0 0 5.82 0 13s5.82 13 13 13Z"
+    />
+    <path
+      fill="#0080FF"
+      fillRule="evenodd"
+      d="M20.28 23.92c0-3.734-3.26-6.76-7.28-6.76-4.02 0-7.28 3.026-7.28 6.76 0 0 2.297 2.08 7.28 2.08 4.983 0 7.28-2.08 7.28-2.08Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#03519E"
+      fillRule="evenodd"
+      d="M16.12 17.68c0 1.723-1.397 2.6-3.12 2.6s-3.12-.877-3.12-2.6a8.467 8.467 0 0 1 3.12-.52 8.466 8.466 0 0 1 3.12.52Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#EE8C3C"
+      d="M15.08 15.6a2.08 2.08 0 0 0-4.16 0v1.56a2.08 2.08 0 0 0 4.16 0V15.6Z"
+    />
+    <path
+      fill="#FECBA1"
+      fillRule="evenodd"
+      d="M13.256 4.29c2.893 0 5.239 2.765 5.239 6.175 0 3.41-2.346 6.175-5.24 6.175-2.893 0-5.238-2.765-5.238-6.175 0-3.41 2.345-6.175 5.239-6.175Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#263A5B"
+      fillRule="evenodd"
+      d="M7.882 11.31 9.36 8.84l5.72-2.08 2.08 2.08 1.335 3.25s1.883-8.45-5.239-8.45-5.374 7.67-5.374 7.67Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+export default MenIcon;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import Search from "antd/es/input/Search";
+import {
+  MenIcon,
+  NotificationsIcon,
+  SettingsIcon,
+  UsIcon,
+} from "@/assets/icons";
+import Modal from "./modal";
+
+const onSearch = (value, _e, info) => console.log(info?.source, value);
+
+const Header = () => {
+  const [one, setOne] = useState("none");
+  const [two, setTwo] = useState("none");
+  const [three, setThree] = useState("none");
+  const [finder, setFinder] = useState(0);
+
+  const allClose = () => {
+    setOne("none");
+    setTwo("none");
+    setThree("none");
+    setFinder(0);
+  };
+
+  const oneOpen = () => {
+    setOne("flex");
+    setTwo("none");
+    setThree("none");
+    setFinder(1);
+  };
+
+  const twoOpen = () => {
+    setOne("none");
+    setTwo("flex");
+    setThree("none");
+    setFinder(2);
+  };
+
+  const threeOpen = () => {
+    setOne("none");
+    setTwo("none");
+    setThree("flex");
+    setFinder(3);
+  };
+
+  return (
+    <div className="header">
+      <Search
+        placeholder="input search text"
+        allowClear
+        onSearch={onSearch}
+        style={{
+          width: 200,
+        }}
+      />
+
+      <ul className="header-infobar">
+        <li className="header-infobar__item" >
+          <SettingsIcon />
+        </li>
+
+        <li className="header-infobar__item" onClick={oneOpen}>
+          <NotificationsIcon />
+          <Modal condition={one} allClose={allClose} finder={finder} />
+        </li>
+
+        <li className="header-infobar__item" onClick={twoOpen}>
+          <UsIcon />
+          <Modal condition={two} allClose={allClose} finder={finder} />
+        </li>
+
+        <li className="header-infobar__item" onClick={threeOpen}>
+          <MenIcon />
+          <Modal condition={three} allClose={allClose} finder={finder} />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
